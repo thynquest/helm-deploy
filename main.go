fdba5319996ad6e712cd33798d23dbc883e49f7f
Franck Kambiwa <franck.kambiwa@synepsys.com>
2020-10-03 18:58:20 +0200

    set action config

--- main.go
+++ main.go
@@ -1,16 +1,72 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
 	"os"
+	"strings"
 
+	"github.com/spf13/cobra"
 	"github.com/thynquest/helm-deploy/cmd/helmdeploy"
+	"github.com/thynquest/helm-deploy/manager"
 	"helm.sh/helm/v3/pkg/action"
+	kubefake "helm.sh/helm/v3/pkg/kube/fake"
+	"helm.sh/helm/v3/pkg/release"
+	"helm.sh/helm/v3/pkg/storage/driver"
+	"sigs.k8s.io/yaml"
 )
 
 func main() {
 	actionConfig := new(action.Configuration)
+	cobra.OnInitialize(func() {
+		helmDriver := os.Getenv("HELM_DRIVER")
+		if err := actionConfig.Init(manager.Settings.RESTClientGetter(), manager.Settings.Namespace(), helmDriver, manager.Debug); err != nil {
+			log.Fatal(err)
+		}
+		if helmDriver == "memory" {
+			loadReleasesInMemory(actionConfig)
+		}
+	})
 	cmd := helmdeploy.NewDeployCmd(actionConfig, os.Stdout)
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+// This function loads releases into the memory storage if the
+// environment variable is properly set.
+func loadReleasesInMemory(actionConfig *action.Configuration) {
+	filePaths := strings.Split(os.Getenv("HELM_MEMORY_DRIVER_DATA"), ":")
+	if len(filePaths) == 0 {
+		return
+	}
+
+	store := actionConfig.Releases
+	mem, ok := store.Driver.(*driver.Memory)
+	if !ok {
+		// For an unexpected reason we are not dealing with the memory storage driver.
+		return
+	}
+
+	actionConfig.KubeClient = &kubefake.PrintingKubeClient{Out: ioutil.Discard}
+
+	for _, path := range filePaths {
+		b, err := ioutil.ReadFile(path)
+		if err != nil {
+			log.Fatal("Unable to read memory driver data", err)
+		}
+
+		releases := []*release.Release{}
+		if err := yaml.Unmarshal(b, &releases); err != nil {
+			log.Fatal("Unable to unmarshal memory driver data: ", err)
+		}
+
+		for _, rel := range releases {
+			if err := store.Create(rel); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	// Must reset namespace to the proper one
+	mem.SetNamespace(manager.Settings.Namespace())
 }
